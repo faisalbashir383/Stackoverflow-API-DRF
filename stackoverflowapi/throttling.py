273dd363b77cf1c1e3afb28ebedcb1289c4d3c62
Faisal Ahmad <51709594+faisalbashir383@users.noreply.github.com>
2021-07-26 15:06:05 +0530

    Update throttling.py

--- stackoverflowapi/throttling.py
+++ stackoverflowapi/throttling.py
@@ -1,4 +1,4 @@
 from rest_framework.throttling import UserRateThrottle
 
-class FaisalRateThrottle(UserRateThrottle):
-    scope='faisal'
+class UserMinThrottle(UserRateThrottle):
+             scope = 'user_min'
